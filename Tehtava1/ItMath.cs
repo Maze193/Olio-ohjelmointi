2f2567e0f28a71499d974cad7351c510b02a744c
L4072 <L4072@student.jamk.fi>
2018-01-25 18:39:26 +0200

    Started homework

---
+++ Tehtava1/ItMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava1
+{
+    class ItMath
+    {
+        
+        public static bool OnkoLuku(string syote)
+        {
+            bool ol;
+            int luku = 0;
+            ol = int.TryParse(syote, out luku);
+            return ol;
+        }
+    }
+}
---
+++ Tehtava1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ItMath math = new ItMath();
+            string str;
+
+            Console.WriteLine("Give a string to see if it is a number");
+            str = Console.ReadLine();
+
+            math.OnkoLuku() = str;
+
+            if (math.OnkoLuku() = true)
+            {
+
+            }
+        }
+    }
+}
