98cac289979fede95e8c73fd74da32c015b84cb3
L4072 <L4072@student.jamk.fi>
2018-02-12 16:28:12 +0200

    Exeption homework started

---
+++ Exeptions2/Names.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exeptions2
+{
+    class Names
+    {
+        List<string> namelist = new List<string>();
+        int amount, amount2;
+        public void NamesList()
+        {
+            string text = @"D:\L4072\nimet.txt";
+            if(File.Exists(text) == true)
+            {
+                var texti = File.ReadAllLines(@"D:\L4072\nimet.txt");
+                foreach (string name in texti)
+                {
+                    namelist.Add(name);
+                    
+                    amount++;
+                }
+
+                var duplicates = namelist
+                    .GroupBy(i => i)
+                    .Where(g => g.Count() > 0)
+                    .Select(g => g.Key);
+                foreach (var d in duplicates)
+                    Console.WriteLine(d + d.Count()); // 4,3
+            }
+        }
+    }
+}
