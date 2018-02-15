af52265749facfbe9669d9368e144922156aea2f
L4072 <L4072@student.jamk.fi>
2018-02-15 17:43:55 +0200

    Delegate2 continued

--- Delegaatit2/MailBook.cs
+++ Delegaatit2/MailBook.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Delegaatit2
 {
     class MailBook:Friend
     {
+
+        string tutut = @"tutut.csv";
+
+        MailBook()
+        {
+            List<Friend> friends = new List<Friend>();
+
+            using (StreamReader str = new StreamReader(tutut))
+            {
+
+            }
+
+        }
 
     }
 }
--- Delegaatit2/Program.cs
+++ Delegaatit2/Program.cs
@@ -10,6 +10,8 @@ namespace Delegaatit2
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
