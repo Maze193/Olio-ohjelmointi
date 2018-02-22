fde48d38c912dcee83e6742a810993a60882d653
Marko Sydänmaa <skyris@hotmail.fi>
2018-02-22 10:21:16 +0200

    Homework mosty done

---
+++ Kertaus5/ArrayCalcs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kertaus5
+{
+    class ArrayCalcs
+    {
+        
+
+        static public double Sum(double[] arr)
+        {
+            double z = arr.Sum();
+            return z;
+        }
+        static public double Average(double[] arr)
+        {
+            double z = arr.Average();
+            return z;
+        }
+        static public double Min(double[] arr)
+        {
+            double z = arr.Min();
+            return z;
+        }
+        static public double Max(double[] arr)
+        {
+            double z = arr.Max();
+            return z;
+        }
+    }
+}
--- Kertaus5/Program.cs
+++ Kertaus5/Program.cs
@@ -10,6 +10,13 @@ namespace Kertaus5
     {
         static void Main(string[] args)
         {
+            double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+
+            Console.WriteLine("Sum = " + ArrayCalcs.Sum(array));
+            Console.WriteLine("Ave = " + ArrayCalcs.Average(array));
+            Console.WriteLine("Min = " + ArrayCalcs.Min(array));
+            Console.WriteLine("Max = " + ArrayCalcs.Max(array));
+
         }
     }
 }
